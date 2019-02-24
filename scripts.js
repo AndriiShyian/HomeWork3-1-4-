f69b012762bc7f9a3d79550a3798be0daad241d3
Andrii Shyian <andrew.shyian.87@gmail.com>
2019-02-24 15:30:40 +0200

    HW3 (1-4 tasks)

---
+++ scripts.js
@@ -0,0 +1,86 @@
+// task1(for)
+// let end = 200;
+// for(let i = 0; i <= end; i++){
+// if(i % 2 === 0 && i != 0){
+//     console.log(i);
+// }
+// }
+
+
+// task1 (while)
+// let i =0;
+// while(i <= 200){
+//     if(i % 2 === 0 && i != 0){ 
+//     console.log(i);}
+//     i++;
+// }
+
+
+// task1 (do...while)
+// let i = 0;
+// do {
+// if(i % 2 === 0 && i != 0){
+//     console.log(i);
+// }
+// i++;
+// }
+// while(i <= 200);
+
+
+// task2(for)
+// const age = 19;
+// for(let i = 0; i < 1000; i++){
+//     if(i % age === 0 && i != 0){
+//         console.log(i);
+//     }
+// }
+
+
+// task2(while)
+// const age = 19;
+// let i = 0;
+// while(i<1000){
+//     if(i % age === 0 && i != 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+
+ //task3
+//let list = [4, 5, 100, 2, 4, 12, -4, 64, -340, 24, 5, -87];
+ //console.log(list);
+ //console.log('Min argument '+minNumber(list));
+ //console.log('Max element '+maxNumber(list));
+
+ //function minNumber(list){
+     //let min = list[0];
+    // for(let i = 0; i < list.length; i++){
+         //if(min > list[i]){
+       //      min = list[i];
+     //    }
+   //  }
+ //return min;
+ //}
+
+// function maxNumber(list){
+//     let max = list[0];
+//     for(let i = 0; i < list.length; i++){
+//         if(max < list[i]){
+//             max = list[i];
+//         }
+//     }
+//     return max;
+// }
+
+
+
+
+//let list = 'Якщо ви не здаєтеся, це має значення';
+//console.log(list);
+//console.log(splitWords(list));
+//function splitWords(list){
+  //   let l = list.split(' ');
+    //return l;
+//}
